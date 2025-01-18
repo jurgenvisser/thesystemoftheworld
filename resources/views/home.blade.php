1f25ea1ccf4850f1f3d3b817f010557f1cc7bd7f
Jurgen Visser <jvisser31@noorderpoort.nl>
2025-01-18 21:55:45 +0100

    edited homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,6 +24,29 @@
                 </div>
             </div>
         </div>
+
+        <div class="flex flex-row h-screen">
+            <!-- Other content above -->
+            <div class="flex-1 flex items-center justify-center">
+                <div class="h-auto w-96 ml-6 bg-black">
+                    {!! file_get_contents(public_path('images/TheSystemFull.svg')) !!}
+                    
+                </div>
+                {{-- <div class="flex-1 bg-black font-times text-xl capitalize text-white p-8">
+                    <div>
+                        We’re working hard behind the scenes to create something amazing for you! Our website is currently under development and will be live very soon.
+                    </div>
+                    <div class="py-3">
+                        In the meantime, stay connected and get exclusive updates by following us on TikTok: <span class="underline decoration-systemYellow">@thesystemoftheworld</span>.
+                    </div>
+                    <div>
+                        Thank you for your patience – we can’t wait to share what’s coming next!
+                    </div>
+                </div> --}}
+            </div>
+            
+            <!-- Other content below -->
+          </div>
         {{-- <h1 class="text-white">Welcome to my Blade Template!</h1> --}}
     </div>
 </body>
