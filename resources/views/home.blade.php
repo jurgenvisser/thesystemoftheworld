1a01069f834f0c9ccd4ebdf5eb44bbcea8aad3b6
Jurgen Visser <jvisser31@noorderpoort.nl>
2025-01-19 01:55:24 +0100

    finalised coming soon landing page second time

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,6 +38,9 @@
                 We’re working hard behind the scenes to create something amazing for you! Our website is currently under development and will be live very soon. <br>
                 In the meantime, stay connected and get exclusive updates by following us on TikTok: <a href="https://www.tiktok.com/@thesystemoftheworld" target="_blank" class="underline decoration-systemYellow">@thesystemoftheworld</a>. <br>
                 Thank you for your patience – we can’t wait to share what’s coming next!
+                <div class="w-full bg-black block lg:hidden pt-2 lg:pt-0">
+                    {!! file_get_contents(public_path('images/TheSystemCrypto.svg')) !!}
+                </div>
             </div>      
         </div>
     </div>
