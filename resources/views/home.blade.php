3af1142bf8f1ae5b39efca87289d298887c62f22
Jurgen Visser <jurgenbv@gmail.com>
2025-01-19 02:10:31 +0100

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,8 @@
     <title>thesystemoftheworld.com</title>
 </head>
 <body>
-    <div class="h-screen bg-systemYellow flex flex-col">
-        <!-- Header Section -->
-        <div class="bg-black h-16 flex items-center justify-center lg:justify-between px-8 text-white">
+    <div class="h-screen bg-yellow-400">
+        <div class="bg-black h-16 flex items-center justify-between px-8 text-white">
             <div class="flex items-center">
                 <div class="h-auto w-12">
                     {!! file_get_contents(public_path('images/TheSystemHorse.svg')) !!}
@@ -18,29 +17,14 @@
                     {!! file_get_contents(public_path('images/TheSystemText.svg')) !!}
                 </div>
             </div>
-             <div class="font-times text-3xl hidden lg:block">COMING SOON...</div>
+            <div class="font-times text-3xl">COMING SOON...</div>
             <div>
-                <div class="h-auto w-60 ml-6 hidden lg:block">
+                <div class="h-auto w-60 ml-6">
                     {!! file_get_contents(public_path('images/TheSystemCrypto.svg')) !!}
                 </div>
             </div>
         </div>
-
-        <!-- Main Content Section -->
-        <div class="flex-1 flex items-center justify-center flex-col lg:flex-row">
-            <!-- Image Section -->
-            <div class="w-96 bg-black">
-                {!! file_get_contents(public_path('images/TheSystemFull.svg')) !!}
-            </div>
-
-            <!-- Text Content Section -->
-            <div class="bg-black font-times text-xl text-white p-8 h-96 w-96 mt-8 lg:mt-0 lg:ml-8 text-center lg:text-left">
-                We’re working hard behind the scenes to create something amazing for you! Our website is currently under development and will be live very soon. <br>
-                In the meantime, stay connected and get exclusive updates by following us on TikTok: <span class="underline decoration-systemYellow">@thesystemoftheworld</span>. <br>
-                Thank you for your patience – we can’t wait to share what’s coming next!
-            </div>      
-        </div>
+        {{-- <h1 class="text-white">Welcome to my Blade Template!</h1> --}}
     </div>
 </body>
-            
 </html>
